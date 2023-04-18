ba9a16eec39c44620311ec798317bd935925fa65
Baloohoney <baloohoney@gmail.com>
2023-04-18 21:38:23 +0300

    Add updatePost test

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -15,4 +15,20 @@ class WallServiceTest {
         val result = WallService.add(post)
         assertEquals(1, result.id)
     }
+
+    @Test
+    fun updateExistsPostTrue(): Post {
+        val post = Post()
+        val result = WallService.update(post)
+        assertEquals(true, result)
+        return post
+    }
+
+    @Test
+    fun updateExistsPostFalse(): Post? {
+        val post = Post()
+        val result = WallService.update(post)
+        assertEquals(false, result)
+        return null
+    }
 }
